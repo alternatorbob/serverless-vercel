c15e1e19cd8e4ec8c7cea97fb41b932913d4ceda
alternatorbob <55140092+alternatorbob@users.noreply.github.com>
2023-03-09 11:28:27 +0100

    first commit

--- main.js
+++ main.js
@@ -1,70 +1,75 @@
-import './style.css'
-import { inPaint } from './replicate'
+import "./style.css";
+import { inPaint } from "./replicate";
 
-const canvas = document.querySelector('#canvas');
-const imgResults = document.querySelector('#result');
-const ctx = canvas.getContext('2d');
+const canvas = document.querySelector("#canvas");
+const imgResults = document.querySelector("#result");
+const ctx = canvas.getContext("2d");
 
-const form = document.querySelector('#form')
+const form = document.querySelector("#form");
 
 const imageInput = form.querySelector('input[type="file"]');
-let canvas64, mask64
+let canvas64, mask64;
 
 imageInput.onchange = (e) => {
-  const file = e.target.files[0];
+    const file = e.target.files[0];
 
-  // check if file empty
-  const reader = new FileReader();
+    // check if file empty
+    const reader = new FileReader();
 
+    reader.onload = (e) => {
+        const img = new Image();
+        img.onload = () => {
+            const min = Math.min(img.width, img.height);
+            canvas.width = min;
+            canvas.height = min;
 
-  reader.onload = (e) => {
-    const img = new Image();
-    img.onload = () => {
-      const min = Math.min(img.width, img.height);
-      canvas.width = 512;
-      canvas.height = 512;
+            ctx.save();
 
-      ctx.save()
+            const ratio = img.width / img.height;
 
-      const ratio = img.width / img.height;
+            ctx.drawImage(img, 0, 0, canvas.width, canvas.height / ratio);
 
-      ctx.drawImage(img, 0, 0, canvas.width, canvas.height / ratio);
+            canvas64 = canvas.toDataURL();
 
-      canvas64 = canvas.toDataURL();
+            // force clear canvas by resizing it
+            // canvas.width = canvas.width;
 
-      // force clear canvas by resizing it
-      canvas.width = canvas.width;
+            // ctx.fillStyle = "white";
+            // ctx.fillRect(0, 0, canvas.width, canvas.height);
+            
+            ctx.beginPath();
+            let radius = canvas.width * 0.3;
+            ctx.arc(
+                canvas.width / 2,
+                canvas.height / 2,
+                radius,
+                0,
+                2 * Math.PI
+            );
+            ctx.fillStyle = "black";
+            ctx.fill();
+            ctx.restore();
 
-      ctx.fillStyle = 'white';
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
-      ctx.beginPath();
-      let radius = canvas.width * 0.3;
-      ctx.arc(canvas.width / 2, canvas.height / 2, radius, 0, 2 * Math.PI);
-      ctx.fillStyle = 'black';
-      ctx.fill();
-      ctx.restore()
-
-      mask64 = canvas.toDataURL();
+            mask64 = canvas.toDataURL();
+        };
+        img.src = e.target.result;
+    };
+    if (file) {
+        reader.readAsDataURL(file);
     }
-    img.src = e.target.result;
-  }
-  if (file) {
-    reader.readAsDataURL(file);
-  }
-}
+};
 
 form.onsubmit = async (e) => {
+    e.preventDefault();
 
-  e.preventDefault();
+    // create formdata
+    // canvas to file
 
-  // create formdata
-  // canvas to file
+    const output = await inPaint(canvas64, mask64, (value) => {
+        console.log("progression:", value);
+    });
 
-  const output = await inPaint(canvas64, mask64, (value) => {
-    console.log('progression:', value);
-  });
-
-  const img = new Image()
-  img.src = output;
-  imgResults.appendChild(img)
-}
+    const img = new Image();
+    img.src = output;
+    imgResults.appendChild(img);
+};
